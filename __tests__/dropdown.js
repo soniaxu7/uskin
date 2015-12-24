48b884c1313c198ad12341d0d0ed2318c0112e78
xuyina <xuyina@unitedstack.com>
2015-12-24 15:04:25 +0800

    add component dropdown and test case

--- __tests__/dropdown.js
+++ __tests__/dropdown.js
@@ -4,25 +4,118 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-const CheckboxWithLabel = require('../js/components/dropdown.jsx');
+const Dropdown = require('../js/components/dropdown.jsx').default;
 
 describe('CheckboxWithLabel', () => {
 
-  it('changes the text after click', () => {
+  var listener = jest.genMockFunction();
+  var items = [{
+    title: 'Basic Ops',
+    key: 'basic',
+    items: [{
+      title: 'Reboot',
+      key: '0',
+      onClick: listener
+    }, {
+      title: 'Take Image Snapshot',
+      key: '1',
+      onClick: listener
+    }]
+  }, {
+    title: 'Network Ops',
+    key: 'network',
+    items: [{
+      title: 'Associate Public IP',
+      key: '2',
+      onClick: listener
+    }, {
+      title: 'Dissociate Public IP',
+      key: '3',
+      onClick: listener
+    }, {
+      title: 'Join Networks',
+      key: '4',
+      onClick: listener
+    }]
+  }, {
+    items: [{
+      title: 'Change Security Group',
+      key: '5',
+      onClick: listener
+    }, {
+      title: 'Change Passoword',
+      key: '6',
+      onClick: listener
+    }, {
+      title: 'Change Keypair',
+      key: '7',
+      onClick: listener
+    }]
+  }, {
+    title: 'Volume Ops',
+    key: 'volume',
+    items: [{
+      title: 'Add Volume',
+      key: '8',
+      onClick: listener
+    }, {
+      title: 'Remove Volume',
+      key: '9',
+      disabled: true,
+      onClick: listener
+    }]
+  }, {
+    title: 'Dangerous Ops',
+    key: 'dangerous',
+    items: [{
+      title: 'Termitate',
+      key: '10',
+      danger: true,
+      onClick: listener
+    }]
+  }];
 
-    // Render a checkbox with label in the document
-    var checkbox = TestUtils.renderIntoDocument(
-      <CheckboxWithLabel labelOn="On" labelOff="Off" />
+  it('should render a dropdown with items', () => {
+
+    var dropdown = TestUtils.renderIntoDocument(
+      <Dropdown items={items}/>
     );
+    var dropdownNode = ReactDOM.findDOMNode(dropdown);
 
-    var checkboxNode = ReactDOM.findDOMNode(checkbox);
+    var blockNodeSum = dropdownNode.childNodes.length;
+    var itemNodeSum = TestUtils.scryRenderedDOMComponentsWithTag(dropdown, 'li').length;
 
-    // Verify that it's Off by default
-    expect(checkboxNode.textContent).toEqual('Off');
+    var itemsSum = 0;
+    items.map(block => {
+      if (block.title) {
+        itemsSum++;
+      }
+      itemsSum += block.items.length;
+    });
 
-    // Simulate a click and verify that it is now On
-    TestUtils.Simulate.change(TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input'));
-    expect(checkboxNode.textContent).toEqual('On');
+    expect(blockNodeSum).toEqual(items.length);
+    expect(itemNodeSum).toEqual(itemsSum);
+
+  });
+
+  it('should return item data when clicked and should not trigger click when item is disabled', () => {
+
+    var dropdown = TestUtils.renderIntoDocument(
+      <Dropdown items={items}/>
+    );
+    var dropdownNode = ReactDOM.findDOMNode(dropdown);
+
+    var clickIndex = [1, 2];
+    var clickNode = dropdownNode.childNodes[clickIndex[0]].childNodes[clickIndex[1] + 1];
+
+    var disabledIndex = [3, 1];
+    var disabledNode = dropdownNode.childNodes[disabledIndex[0]].childNodes[disabledIndex[1] + 1];
+
+    TestUtils.Simulate.click(clickNode);
+    TestUtils.Simulate.click(disabledNode);
+
+    expect(listener.mock.calls[0][1]).toEqual(items[clickIndex[0]].items[clickIndex[1]]);
+    expect(listener.mock.calls.length).toBe(1);
   });
 
 });
---
+++ docs/react_demos/dist/dropdown.js
@@ -0,0 +1,185 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var Dropdown = uskin.Dropdown;
+
+	//key value should be exclusive
+	var items1 = [{
+	  items: [{
+	    title: 'Reboot',
+	    key: '0',
+	    onClick: listener
+	  }, {
+	    title: 'Take Image Snapshot',
+	    key: '1',
+	    onClick: listener
+	  }]
+	}, {
+	  items: [{
+	    title: 'Associate Public IP',
+	    key: '2',
+	    onClick: listener
+	  }, {
+	    title: 'Dissociate Public IP',
+	    key: '3',
+	    onClick: listener
+	  }, {
+	    title: 'Join Networks',
+	    key: '4',
+	    onClick: listener
+	  }]
+	}, {
+	  items: [{
+	    title: 'Change Security Group',
+	    key: '5',
+	    onClick: listener
+	  }, {
+	    title: 'Change Passoword',
+	    key: '6',
+	    onClick: listener
+	  }, {
+	    title: 'Change Keypair',
+	    key: '7',
+	    onClick: listener
+	  }]
+	}, {
+	  items: [{
+	    title: 'Add Volume',
+	    key: '8',
+	    onClick: listener
+	  }, {
+	    title: 'Remove Volume',
+	    key: '9',
+	    disabled: true,
+	    onClick: listener
+	  }]
+	}, {
+	  items: [{
+	    title: 'Termitate',
+	    key: '10',
+	    danger: true,
+	    onClick: listener
+	  }]
+	}];
+
+	var items2 = [{
+	  title: 'Basic Ops',
+	  key: 'basic',
+	  items: [{
+	    title: 'Reboot',
+	    key: '0',
+	    onClick: listener
+	  }, {
+	    title: 'Take Image Snapshot',
+	    key: '1',
+	    onClick: listener
+	  }]
+	}, {
+	  title: 'Network Ops',
+	  key: 'network',
+	  items: [{
+	    title: 'Associate Public IP',
+	    key: '2',
+	    onClick: listener
+	  }, {
+	    title: 'Dissociate Public IP',
+	    key: '3',
+	    onClick: listener
+	  }, {
+	    title: 'Join Networks',
+	    key: '4',
+	    onClick: listener
+	  }]
+	}, {
+	  items: [{
+	    title: 'Change Security Group',
+	    key: '5',
+	    onClick: listener
+	  }, {
+	    title: 'Change Passoword',
+	    key: '6',
+	    onClick: listener
+	  }, {
+	    title: 'Change Keypair',
+	    key: '7',
+	    onClick: listener
+	  }]
+	}, {
+	  title: 'Volume Ops',
+	  key: 'volume',
+	  items: [{
+	    title: 'Add Volume',
+	    key: '8',
+	    onClick: listener
+	  }, {
+	    title: 'Remove Volume',
+	    key: '9',
+	    disabled: true,
+	    onClick: listener
+	  }]
+	}, {
+	  title: 'Dangerous Ops',
+	  key: 'dangerous',
+	  items: [{
+	    title: 'Termitate',
+	    key: '10',
+	    danger: true,
+	    onClick: listener
+	  }]
+	}];
+
+	function listener(e, status) {
+	  console.debug('click triggered!', e, status);
+	}
+
+	ReactDOM.render(React.createElement(Dropdown, { items: items1 }), document.getElementById('example'));
+
+	ReactDOM.render(React.createElement(Dropdown, { items: items2 }), document.getElementById('example2'));
+
+/***/ }
+/******/ ]);
