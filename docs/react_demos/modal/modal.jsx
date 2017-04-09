744527fbdb37060bf710aa5bb24c018cbf9d7278
sonia xu <soniaxu7@foxmail.com>
2017-04-09 09:31:28 +0800

    remove modal with tip
    Change-Id: I161d45cd8e4df9d41ff0356295d8d5bbb946705f

--- docs/react_demos/modal/modal.jsx
+++ docs/react_demos/modal/modal.jsx
@@ -1,17 +1,8 @@
 const {Button, Modal} = uskin;
 
-function listener(v) {
-  let props = {
-    title: v.toUpperCase(),
-    content: 'this is my modal1',
-    okText: 'OK'
-  };
-
-  Modal[v](props);
-}
-
 function listener2(v) {
   let props = {
+    title: v.toUpperCase(),
     content: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language. Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications.',
     okText: 'OK'
   };
@@ -21,20 +12,10 @@ function listener2(v) {
 
 ReactDOM.render(
   <div>
-    <p>Modal with title</p>
-    <div>
-      <Button value="Info Caller" btnKey="info" onClick={listener.bind(this, 'info')} />
-      <Button value="Success Caller" type="create" btnKey="success" onClick={listener.bind(this, 'success')} />
-      <Button value="Warning Caller" type="warning" btnKey="warning" onClick={listener.bind(this, 'warning')} />
-      <Button value="Danger Caller" type="delete" btnKey="danger" onClick={listener.bind(this, 'danger')} />
-    </div>
-    <p>Modal with no title</p>
-    <div>
-      <Button value="Info Caller" btnKey="info" onClick={listener2.bind(this, 'info')} />
-      <Button value="Success Caller" type="create" btnKey="success" onClick={listener2.bind(this, 'success')} />
-      <Button value="Warning Caller" type="warning" btnKey="warning" onClick={listener2.bind(this, 'warning')} />
-      <Button value="Danger Caller" type="delete" btnKey="danger" onClick={listener2.bind(this, 'danger')} />
-    </div>
+    <Button value="Info Caller" btnKey="info" onClick={listener2.bind(this, 'info')} />
+    <Button value="Success Caller" type="create" btnKey="success" onClick={listener2.bind(this, 'success')} />
+    <Button value="Warning Caller" type="warning" btnKey="warning" onClick={listener2.bind(this, 'warning')} />
+    <Button value="Danger Caller" type="delete" btnKey="danger" onClick={listener2.bind(this, 'danger')} />
   </div>,
   document.getElementById('example')
 );
--- js/components/modal/index.jsx
+++ js/components/modal/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Modal from './modal';
-import Tip from '../tip/index';
 import shim from './shim';
 
-let btnMap = {
+const btnMap = {
   danger: 'delete',
   success: 'create',
   warning: 'warning'
 };
 
-let iconMap = {
+const iconMap = {
   danger: 'deleted',
   info: 'warning',
   success: 'active',
@@ -18,27 +17,28 @@ let iconMap = {
 
 ['info', 'success', 'warning', 'danger'].forEach((m) => {
   Modal[m] = function(props) {
-    let content;
-
-    if (props.title) {
-      content = <Tip content={props.content} type={m} />;
-    } else {
-      content = (
-        <div className="modal-reminder-content">
-          <div className="modal-reminder-title">
-            <i className={'glyphicon icon-status-' + iconMap[m] + ' ' + m} />
-            <span>{m.charAt(0).toUpperCase() + m.slice(1)}</span>
-          </div>
-          <div className="modal-reminder-message">
-            {props.content}
-          </div>
+    let content = (
+      <div className="modal-reminder-content">
+        <div className="modal-reminder-title">
+          <i className={'glyphicon icon-status-' + iconMap[m] + ' ' + m} />
+          <span>
+          {
+            props.title ?
+              props.title
+            : m.charAt(0).toUpperCase() + m.slice(1)
+          }
+          </span>
         </div>
-      );
-    }
+        <div className="modal-reminder-message">
+          {props.content}
+        </div>
+      </div>
+    );
 
     let _props = Object.assign({}, props, {
       content: content,
-      btnType: btnMap[m]
+      btnType: btnMap[m],
+      title: undefined
     });
     return shim(_props);
   };
